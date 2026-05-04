cbb7532aee3dca42cd72a0627dbb3c8d5310cb4e
Jiumengzz <josedanielagudelo6@gmail.com>
2026-05-04 05:25:31 -0400

    yeehaw

--- Src/main.c
+++ Src/main.c
@@ -20,7 +20,8 @@ void set_led(int32_t pin_num, int32_t val);
 int main(void)
 {
     init_led();
-    set_led(6,1); // turn on LED
+    // set_led(6,1); // turn on LED
+    GPIOC-> BSRR |= 1 << 6;
 
 
 
@@ -32,12 +33,12 @@ int main(void)
 void init_led(void) {
     RCC-> AHB2ENR |= RCC_AHB2ENR_GPIOCEN; // enable GPIOC clock
     GPIOC-> MODER &= ~0x00002000; // reset register
-    GPIOC-> MODER |= 0x000001000;
+    GPIOC-> MODER |= 0x00001000;
     // GPIOC-> MODER &= ~GPIO_MODER_MODER6_1; 
     // GPIOC-> MODER |= GPIO_MODER_MODER6_0; // PC6 01 output
-    GPIOC-> OTYPER &= GPIO_OTYPER_OT6; // PC6 push-pull mode 
+    GPIOC-> OTYPER &= ~GPIO_OTYPER_OT6; // PC6 push-pull mode 
 }
 
-void set_led(int32_t pin_num, int32_t val) {
-    GPIOC-> BSRR &= ~(1 << pin_num); 
-}
+// void set_led(int32_t pin_num, int32_t val) {
+//     GPIOC-> BSRR &= ~(1 << pin_num); 
+// }
